e5284ed87971eedd7827492bae5285b158511dae
Orinks <orin8722@gmail.com>
2025-05-21 20:59:32 -0400

    Update version to 0.9.3 for dev branch

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="accessiweather",
-    version="0.9.2",
+    version="0.9.3",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     install_requires=[
